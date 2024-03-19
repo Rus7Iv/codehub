ede84e00f835cbb0a5a8df4d7732dd3a8b7f3572
Ruslan Ivanov <rusivary@gmail.com>
2024-03-20 00:39:03 +0400

    added prettier rules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,21 +9,27 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture =
+      TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'codehub' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture =
+      TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('codehub');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture =
+      TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, codehub');
+    const compiled =
+      fixture.nativeElement as HTMLElement;
+    expect(
+      compiled.querySelector('h1')?.textContent,
+    ).toContain('Hello, codehub');
   });
 });
