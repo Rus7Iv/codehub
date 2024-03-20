7cfc517b81f02313e335322fa62dabbf50faceac
Ruslan Ivanov <rusivary@gmail.com>
2024-03-20 23:20:58 +0400

    added readme visible in repository detail

--- src/app/repository-detail/repository-detail.component.ts
+++ src/app/repository-detail/repository-detail.component.ts
@@ -1,7 +1,20 @@
+// repository-detail.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { IRepository } from '../repository-list/repository-list.component';
+
+interface IContentResponse {
+  content: string;
+}
+
+interface IFile {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-repository-detail',
@@ -10,16 +23,36 @@ import { IRepository } from '../repository-list/repository-list.component';
 })
 export class RepositoryDetailComponent implements OnInit {
   repository: IRepository | null = null;
+  readme: SafeHtml | null = null;
 
   constructor(
     private route: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.http.get<IRepository>(`https://api.github.com/repositories/${id}`).subscribe((data: IRepository) => {
       this.repository = data;
+    });
+
+    this.http.get<IFile[]>(`https://api.github.com/repositories/${id}/contents`).pipe(
+      switchMap((files: IFile[]) => {
+        const readmeFile = files.find(file => file.name.toLowerCase() === 'readme.md');
+        if (readmeFile) {
+          return this.http.get<IContentResponse>(readmeFile.url);
+        } else {
+          console.log('Файл README не найден');
+          return of({ content: '' });
+        }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        console.log('Ошибка при получении файла README:', error.message);
+        return of({ content: '' });
+      })
+    ).subscribe((data: IContentResponse) => {
+      this.readme = this.sanitizer.bypassSecurityTrustHtml(atob(data.content));
     });
   }
 }
