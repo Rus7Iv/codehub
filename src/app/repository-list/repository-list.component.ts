e8769738e139ece4a53e23697d0fb0a5917d40c1
Ruslan Ivanov <rusivary@gmail.com>
2024-03-21 11:03:05 +0400

    added search state

--- src/app/repository-list/repository-list.component.ts
+++ src/app/repository-list/repository-list.component.ts
@@ -3,6 +3,7 @@ import { IRepository } from '../repository-detail/repository-detail.component';
 import { GithubService } from '../../services/github.service';
 import { FormControl } from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { SearchStateService } from '../../services/search-state.service';
 
 @Component({
   selector: 'app-repository-list',
@@ -12,13 +13,19 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 export class RepositoryListComponent implements OnInit {
   repositories: IRepository[] = [];
   searchControl = new FormControl();
-  selectedLanguage: string;
+  selectedLanguage: string = '';
 
-  constructor(private githubService: GithubService) {
-    this.selectedLanguage = '';
-  }
+  constructor(private githubService: GithubService, private searchStateService: SearchStateService) { }
 
   ngOnInit(): void {
+    this.searchStateService.getSearchTerm().subscribe(term => {
+      this.searchControl.setValue(term);
+    });
+
+    this.searchStateService.getSelectedLanguage().subscribe(language => {
+      this.selectedLanguage = language;
+    });
+
     this.githubService.getRepositories().subscribe((data: IRepository[]) => {
       this.repositories = data;
     });
@@ -27,6 +34,7 @@ export class RepositoryListComponent implements OnInit {
       debounceTime(1000),
       distinctUntilChanged()
     ).subscribe(searchTerm => {
+      this.searchStateService.setSearchTerm(searchTerm);
       this.githubService.searchRepositories(searchTerm, this.selectedLanguage).subscribe((data: IRepository[]) => {
         this.repositories = data;
       });
@@ -36,6 +44,7 @@ export class RepositoryListComponent implements OnInit {
   onLanguageSelect(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     this.selectedLanguage = selectElement.value;
+    this.searchStateService.setSelectedLanguage(this.selectedLanguage);
     this.githubService.searchRepositories(this.searchControl.value, this.selectedLanguage).subscribe((data: IRepository[]) => {
       this.repositories = data;
     });
